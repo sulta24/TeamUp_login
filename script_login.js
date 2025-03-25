ba6c384061aabe28ee6b66a96ef288ea0f283faf
sulta24 <155542619+sulta24@users.noreply.github.com>
2025-03-25 18:18:42 +0500

    Update script_login.js

--- script_login.js
+++ script_login.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
         };
 
         try {
-            const response = await fetch("https://4775-37-99-40-85.ngrok-free.app/users", {
+            const response = await fetch("https://2d2e-37-99-40-85.ngrok-free.app/users", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
