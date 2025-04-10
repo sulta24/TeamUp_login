6d2f9836241ac1eb8eef792d41883a59abd08e94
sulta24 <155542619+sulta24@users.noreply.github.com>
2025-04-10 20:50:20 +0500

    Update script_login.js

--- script_login.js
+++ script_login.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
         };
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/users", {
+            const response = await fetch("https://suppose-various-villa-extended.trycloudflare.com/users", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
