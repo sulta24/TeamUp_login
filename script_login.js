1977ff064a985988feda5d68f350d964d509a80e
sulta24 <155542619+sulta24@users.noreply.github.com>
2025-04-13 20:32:19 +0500

    Update script_login.js

--- script_login.js
+++ script_login.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
         };
 
         try {
-            const response = await fetch("https://devil-neighbor-locate-sending.trycloudflare.com/users", {
+            const response = await fetch("https://levels-struct-statement-doctor.trycloudflare.com/users", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
